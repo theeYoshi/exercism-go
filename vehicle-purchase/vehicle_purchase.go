131269bb44cc215661b675e3afc5b2a44a0d7b7d
yoshida <rafael.yoshida@zpesystems.com>
2024-03-18 17:54:26 -0300

    NeedsLicense func done

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,12 +2,18 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" {
+		return true
+	} else if kind == "truck" {
+		return true
+	} else {
+		return false
+	}
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
