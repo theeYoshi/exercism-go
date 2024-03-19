55be84b1be5adebf1e4eb3cccbb124a36cf365bf
yoshida <rafael.yoshida@zpesystems.com>
2024-03-18 21:52:30 -0300

    CalculateResellPrice func done and example vehicle_purschase done

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -24,5 +24,11 @@ func ChooseVehicle(option1, option2 string) string {
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age < 3.00 {
+		return originalPrice * 0.8
+	} else if age < 10.0 {
+		return originalPrice * 0.7
+	} else {
+		return originalPrice * 0.5
+	}
 }
