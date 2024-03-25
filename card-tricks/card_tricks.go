860c33b33aa0396833220e022ccf515c2bc0395d
yoshida <rafael.yoshida@zpesystems.com>
2024-03-25 19:54:38 -0300

    card-tricks FavoriteCards func done

--- card-tricks/card_tricks.go
+++ card-tricks/card_tricks.go
@@ -2,13 +2,14 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	cards := []int{2, 6, 9}
+	return cards
 }
 
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
